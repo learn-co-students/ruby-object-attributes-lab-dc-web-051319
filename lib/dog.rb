f0307af32afc490049a45ec903c2226b61d2b2d5
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-15 15:19:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  # def initialize(name, breed)
+  #    @dog_name = name
+  #    @dog_breed = breed
+  # end
+
+  def name=(doggo_name)
+    @name = doggo_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
